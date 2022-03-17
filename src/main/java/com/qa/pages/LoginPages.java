2cbbef595bb8e45a9a8e38ce949089cc3812a111
harpreetsingh1995 <harpreetsingh.akash@gmail.com>
2022-03-16 20:57:19 -0400

    added search   page featur code

--- src/main/java/com/qa/pages/LoginPages.java
+++ src/main/java/com/qa/pages/LoginPages.java
@@ -4,5 +4,9 @@ public class LoginPages {
 	public void LoginPage() {
 		System.out.println("this is login Page");
 	}
+	public void loginset() {
+		System.out.println("this sset login");
+		
+	}
 
 }
